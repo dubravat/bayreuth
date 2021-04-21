f5701704b78af509b1ea3fb2a927320cece3ed06
dubravat <metro-taras@ukr.net>
2021-04-21 19:26:09 +0200

    final changes

--- task_2/test.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-list = world.name.to_list()
-for i in list:
-    if i.startswith("Si"):
-        print(i)
-singapore = world[world.name == 'Singapore']
-boundaries = singapore['geometry']
